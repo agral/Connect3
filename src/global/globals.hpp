4d7392f780b76a468874749adebd32c6647232c1
agral <adam@gralin.ski>
2018-06-27 23:27:46 +0200

    Add globals file

---
+++ src/global/globals.hpp
@@ -0,0 +1,15 @@
+#ifndef GLOBAL_GLOBALS_HPP
+#define GLOBAL_GLOBALS_HPP
+
+namespace global
+{
+
+const char* WINDOW_TITLE = "Connect3";
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int TARGET_FPS = 60;
+
+} // namespace global
+
+
+#endif // GLOBAL_GLOBALS_HPP
