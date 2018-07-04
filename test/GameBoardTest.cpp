0ad9fff2f42f0d3d00f6c6cb9f8345f8e914ffd6
agral <adam@gralin.ski>
2018-07-05 01:50:07 +0200

    [Test] Check whether chains are marked

--- test/GameBoardTest.cpp
+++ test/GameBoardTest.cpp
@@ -54,4 +54,50 @@ TEST_CASE("Chains of three or more consecutive gems are being found correctly")
   bool areChainsFound = gb.FindChains();
 
   REQUIRE(areChainsFound == true);
+
+  SECTION("Chains of length 3 in the corners are found")
+  {
+    // Chains of length 3 in the corners:
+    CHECK( // Top-left corner, horizontal
+        ((gb.At(0, 0).isPartOfChain) &&
+        (gb.At(0, 1).isPartOfChain) &&
+        (gb.At(0, 2).isPartOfChain))
+    );
+    CHECK( // Top-left corner, vertical
+        ((gb.At(0, 0).isPartOfChain) &&
+        (gb.At(1, 0).isPartOfChain) &&
+        (gb.At(2, 0).isPartOfChain))
+    );
+    CHECK( // Top-right corner, horizontal
+        ((gb.At(0, 5).isPartOfChain) &&
+        (gb.At(0, 6).isPartOfChain) &&
+        (gb.At(0, 7).isPartOfChain))
+    );
+    CHECK( // Top-right corner, vertical
+        ((gb.At(0, 7).isPartOfChain) &&
+        (gb.At(1, 7).isPartOfChain) &&
+        (gb.At(2, 7).isPartOfChain))
+    );
+
+    CHECK( // Bottom-left corner, horizontal
+        ((gb.At(7, 0).isPartOfChain) &&
+        (gb.At(7, 1).isPartOfChain) &&
+        (gb.At(7, 2).isPartOfChain))
+    );
+    CHECK( // Bottom-left corner, vertical
+        ((gb.At(5, 0).isPartOfChain) &&
+        (gb.At(6, 0).isPartOfChain) &&
+        (gb.At(7, 0).isPartOfChain))
+    );
+    CHECK( // Bottom-right corner, horizontal
+        ((gb.At(7, 5).isPartOfChain) &&
+        (gb.At(7, 6).isPartOfChain) &&
+        (gb.At(7, 7).isPartOfChain))
+    );
+    CHECK( // Bottom-right corner, vertical
+        ((gb.At(5, 7).isPartOfChain) &&
+        (gb.At(6, 7).isPartOfChain) &&
+        (gb.At(7, 7).isPartOfChain))
+    );
+  }
 }
