117a884977da484d0ca639942594d34ae79a07cb
agral <adam@gralin.ski>
2018-07-04 20:52:02 +0200

    Add three global consts defining Board geometry

--- src/global/globals.cpp
+++ src/global/globals.cpp
@@ -7,5 +7,8 @@ const char* WINDOW_TITLE = "Connect3";
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const int TARGET_FPS = 60;
+const int GAMEBOARD_HEIGHT = 8;
+const int GAMEBOARD_WIDTH = 8;
+const int ORBS_TYPES_COUNT = 5;
 
 } // namespace global
--- src/global/globals.hpp
+++ src/global/globals.hpp
@@ -8,6 +8,9 @@ extern const char* WINDOW_TITLE;
 extern const int SCREEN_WIDTH;
 extern const int SCREEN_HEIGHT;
 extern const int TARGET_FPS;
+extern const int GAMEBOARD_HEIGHT;
+extern const int GAMEBOARD_WIDTH;
+extern const int ORBS_TYPES_COUNT;
 
 } // namespace global
 
