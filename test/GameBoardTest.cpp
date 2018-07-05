5de65013d24cee21526ed56bc0185b7d95361142
agral <adam@gralin.ski>
2018-07-05 02:25:28 +0200

    [Test] Add bunch of tests for chain detecting func

--- test/GameBoardTest.cpp
+++ test/GameBoardTest.cpp
@@ -10,15 +10,23 @@ std::vector<std::vector<int>> r3 = {
 };
 
 std::vector<std::vector<int>> chains = {
-  {3, 3, 3, 2, 4, 3, 3, 3},
-  {3, 1, 2, 1, 4, 1, 2, 3},
-  {3, 2, 1, 2, 4, 2, 1, 3},
-  {2, 1, 2, 1, 4, 1, 2, 1},
-  {1, 2, 1, 2, 4, 2, 1, 2},
-  {3, 1, 2, 1, 4, 1, 2, 3},
+  {3, 3, 3, 5, 4, 3, 3, 3},
+  {3, 1, 2, 5, 4, 5, 2, 3},
+  {3, 2, 1, 5, 4, 5, 1, 3},
+  {2, 1, 2, 5, 4, 5, 2, 1},
+  {6, 6, 6, 6, 4, 5, 1, 2},
+  {3, 1, 2, 1, 4, 5, 2, 3},
   {3, 2, 1, 2, 4, 2, 1, 3},
   {3, 3, 3, 1, 4, 3, 3, 3},
 };
+
+std::vector<std::vector<int>> tooShortToConstituteChains = {
+  {1, 1, 2, 2},
+  {1, 1, 2, 2},
+  {2, 2, 1, 1},
+  {2, 2, 1, 1}
+};
+
 
 TEST_CASE("Loading Board from a vector works correctly")
 {
@@ -47,6 +55,17 @@ TEST_CASE("Loading Board from a vector works correctly")
   }
 }
 
+
+TEST_CASE("Chains of two consecutive Gems are ignored (not marked as chains)")
+{
+  GameBoard gb;
+  gb.LoadFromVector(tooShortToConstituteChains);
+  bool areChainsFound = gb.FindChains();
+
+  REQUIRE_FALSE(areChainsFound);
+}
+
+
 TEST_CASE("Chains of three or more consecutive gems are being found correctly")
 {
   GameBoard gb;
@@ -55,7 +74,7 @@ TEST_CASE("Chains of three or more consecutive gems are being found correctly")
 
   REQUIRE(areChainsFound == true);
 
-  SECTION("Chains of length 3 in the corners are found")
+  SECTION("Chains of length 3 in the corners are detected")
   {
     // Chains of length 3 in the corners:
     CHECK( // Top-left corner, horizontal
@@ -99,5 +118,86 @@ TEST_CASE("Chains of three or more consecutive gems are being found correctly")
         (gb.At(6, 7).isPartOfChain) &&
         (gb.At(7, 7).isPartOfChain))
     );
+  }
+
+  SECTION("Chains of length 4 are detected")
+  {
+    CHECK(( // Vertical chain at (0, 3) of length 4:
+        (gb.At(0, 3).isPartOfChain) &&
+        (gb.At(1, 3).isPartOfChain) &&
+        (gb.At(2, 3).isPartOfChain) &&
+        (gb.At(3, 3).isPartOfChain)
+    ));
+    CHECK(( // Horizontal chain at (4, 0) of length 4:
+        (gb.At(4, 0).isPartOfChain) &&
+        (gb.At(4, 1).isPartOfChain) &&
+        (gb.At(4, 2).isPartOfChain) &&
+        (gb.At(4, 3).isPartOfChain)
+    ));
+  }
+
+  SECTION("Chains of length 5 are detected")
+  {
+    CHECK(( // Vertical chain at (1, 5) of length 5:
+        (gb.At(1, 5).isPartOfChain) &&
+        (gb.At(2, 5).isPartOfChain) &&
+        (gb.At(3, 5).isPartOfChain) &&
+        (gb.At(4, 5).isPartOfChain) &&
+        (gb.At(5, 5).isPartOfChain)
+    ));
+  }
+
+  // Occasionally it is possible to score a chain of length greater than 5.
+  // Checks whether such a long chain is found:
+  SECTION("Arbitrarily long chains are also detected")
+  {
+    CHECK(( // The chain of fours going from top to bottom of the test board:
+        (gb.At(0, 4).isPartOfChain) &&
+        (gb.At(1, 4).isPartOfChain) &&
+        (gb.At(2, 4).isPartOfChain) &&
+        (gb.At(3, 4).isPartOfChain) &&
+        (gb.At(4, 4).isPartOfChain) &&
+        (gb.At(5, 4).isPartOfChain) &&
+        (gb.At(6, 4).isPartOfChain) &&
+        (gb.At(7, 4).isPartOfChain)
+    ));
+  }
+
+  SECTION("Diagonal chains are ignored")
+  {
+    CHECK_FALSE((
+        (gb.At(5, 1).isPartOfChain) ||
+        (gb.At(6, 2).isPartOfChain) ||
+        (gb.At(7, 3).isPartOfChain)
+    ));
+  }
+
+  SECTION("None of the fields that do not constitute a chain are marked")
+  {
+    CHECK_FALSE((
+        (gb.At(1, 1).isPartOfChain) ||
+        (gb.At(1, 2).isPartOfChain) ||
+        (gb.At(1, 6).isPartOfChain) ||
+        (gb.At(2, 1).isPartOfChain) ||
+        (gb.At(2, 2).isPartOfChain) ||
+        (gb.At(2, 6).isPartOfChain) ||
+        (gb.At(3, 0).isPartOfChain) ||
+        (gb.At(3, 1).isPartOfChain) ||
+        (gb.At(3, 2).isPartOfChain) ||
+        (gb.At(3, 6).isPartOfChain) ||
+        (gb.At(3, 7).isPartOfChain) ||
+        (gb.At(4, 6).isPartOfChain) ||
+        (gb.At(4, 7).isPartOfChain) ||
+        (gb.At(5, 1).isPartOfChain) ||
+        (gb.At(5, 2).isPartOfChain) ||
+        (gb.At(5, 3).isPartOfChain) ||
+        (gb.At(5, 6).isPartOfChain) ||
+        (gb.At(6, 1).isPartOfChain) ||
+        (gb.At(6, 2).isPartOfChain) ||
+        (gb.At(6, 3).isPartOfChain) ||
+        (gb.At(6, 5).isPartOfChain) ||
+        (gb.At(6, 6).isPartOfChain) ||
+        (gb.At(7, 3).isPartOfChain)
+    ));
   }
 }
