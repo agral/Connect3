2b4dd3b8362aaa099b7c85000e98da860d17d69e
agral <adam@gralin.ski>
2018-07-07 16:46:00 +0200

    Introduce a GameTimeData structure
    Adds a new structure to the Gral's Simple Engine that aggregates values
    regarding game time.

---
+++ src/gse/GameTimeData.hpp
@@ -0,0 +1,16 @@
+#ifndef GSE_GAMETIMEDATA_HPP
+#define GSE_GAMETIMEDATA_HPP
+
+namespace gse
+{
+
+struct GameTimeData
+{
+ public:
+  double timeTotal;
+  double timeSinceLastFrame;
+};
+
+} // namespace gse
+
+#endif // GSE_GAMETIMEDATA_HPP
