b5943a7ef2536393056d1358801ab547a9c5cceb
agral <adam@gralin.ski>
2018-07-07 10:55:52 +0200

    Make Gems hold their render position

--- src/Gem.cpp
+++ src/Gem.cpp
@@ -1,7 +1,9 @@
 #include "Gem.hpp"
 
-Gem::Gem(int color) :
+Gem::Gem(int color, double posX, double posY) :
   color(color),
+  posX(posX),
+  posY(posY),
   isPartOfChain(false)
 {
 }
--- src/Gem.hpp
+++ src/Gem.hpp
@@ -4,10 +4,12 @@
 struct Gem
 {
  public:
-  Gem(int color = 0);
+  Gem(int color = 0, double posX = 0.0, double posY = 0.0);
 
  public:
   int color;
+  double posX;
+  double posY;
   bool isPartOfChain;
 };
 
